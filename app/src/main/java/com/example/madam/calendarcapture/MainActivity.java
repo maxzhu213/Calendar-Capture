2878a54790c3dae96739feb7309cb808f508716c
Janujan Gathieswaran <Janujan.g@hotmail.com>
2018-02-09 18:54:18 -0500

    Changes to MainActivity
    Add ability to open camera in order to take a picture.

--- app/src/main/java/com/example/madam/calendarcapture/MainActivity.java
+++ app/src/main/java/com/example/madam/calendarcapture/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.madam.calendarcapture;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
