fe8d8dde268ff711c719a94c8fad2f7fde3377a2
Janujan Gathieswaran <Janujan.g@hotmail.com>
2018-02-15 20:31:36 -0500

    Added button to capture an image and store to device
    Added a button and imageview to the constraint layout to bring up the camera when pressed and display it in the imageview. It also stores the image onto the device.

--- app/src/main/java/com/example/madam/calendarcapture/MainActivity.java
+++ app/src/main/java/com/example/madam/calendarcapture/MainActivity.java
@@ -1,23 +1,116 @@
 package com.example.madam.calendarcapture;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    //Reference for image capture code: https://developer.android.com/training/camera/photobasics.html
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+
+    //used to identify intent
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 2;
+
+    ImageView photo;
+
+    String photoPath;
+
+    //-------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTakePhoto = (Button) findViewById(R.id.takePhoto);
+        photo = (ImageView) findViewById(R.id.photo);
+
+        //disable button if user has no camera
+        if (!hasCamera())
+            btnTakePhoto.setEnabled(false);
     }
+
+    //check if user has a camera
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    //launching the camera
+    public void launchCamera(View view) {
+        //intent to capture a photo
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // Create the file where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+//            // Continue only if the file was successfully created
+//            if (photoFile != null) {
+//                //uri used to identify a resource
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//            }
+
+
+
+
+
+            //take a picture and pass results to onActivityResult
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //ensure that an image was taken and no errors
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extra = data.getExtras();
+
+            //convert data to a bitmap
+            Bitmap imageBitmap = (Bitmap) extra.get("data");
+            photo.setImageBitmap(imageBitmap);
+        }
+
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
